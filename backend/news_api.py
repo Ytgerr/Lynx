15c2000c15b6b1b10df6cba26059d54ebb46bd86
KawaichMag <kawaichmag@gmail.com>
2025-09-21 14:59:48 +0300

    news content parsing added

--- backend/news_api.py
+++ backend/news_api.py
@@ -1,8 +1,19 @@
 import hashlib
+import re
+from asyncio import Timeout
+from sys import stdout
 
+import lxml
+import requests
+from bs4 import BeautifulSoup
 from loguru import logger
 from newsapi import NewsApiClient
 from pymongo import MongoClient
+from readability import Document
+from requests import ReadTimeout
+
+CONTENT_LEN_THRESH = 600
+DEBUG_MODE = 1
 
 CHOSEN_KEYS = [
     "author",
@@ -10,7 +21,50 @@ CHOSEN_KEYS = [
     "description",
     "publishedAt",
     "url",
-]
+]    
+
+REQUEST_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36"
+}
+
+if DEBUG_MODE == 1:
+    logger.remove()
+    logger.add(stdout, level="TRACE")
+
+def get_content(url):
+    try:
+        response = requests.get(url, timeout=5, headers=REQUEST_HEADERS)
+        if response.status_code != 200:
+            logger.error(f"Site {url} has bad request")
+            return None
+
+    except ConnectionError:
+        logger.error(f"Site {url} doesnt responses")
+        return None
+    except ReadTimeout:
+        logger.error(f"Timeout for {url}")
+        return None
+        
+    logger.trace("Response collected")
+
+    readable = Document(response.text)
+
+    soup = BeautifulSoup(readable.summary(), "lxml")
+    siblings = soup.find_all("p")
+
+    # Check for <p> siblings
+    logger.trace(f"Found {len(siblings)} siblings <p> of content")
+    
+    content = ""
+
+    for idx, sibling in enumerate(siblings):
+        if sibling.string:
+            content += "\n" + sibling.string.strip()
+
+        if idx % 3 == 0:
+            logger.trace(f"{idx + 1}/{len(siblings)} collected!")
+
+    return content
 
 hasher = hashlib.sha256()
 client = MongoClient()
@@ -19,28 +73,50 @@ news_collection = db["raw_news"]
 
 api = NewsApiClient(api_key='9c321eb916414dbcad8165d1fda83f45')
 
-result = api.get_top_headlines(category="business")
+def main():
+    result = api.get_top_headlines(category="business")
 
-if result["status"] == "ok":
-    logger.success(f"{result['totalResults']} results recieved!")
+    if result["status"] == "ok":
+        logger.success(f"{result['totalResults']} results recieved!")
 
-    filtered_articles = []
+        filtered_articles = []
 
-    for article in result["articles"]:
-        encoded_title = (article["title"] + article["publishedAt"]).encode("utf-8")
-        hasher.update(encoded_title)
-        article_id = hasher.hexdigest()
+        for article in result["articles"]:
+            encoded_title = (article["title"] + article["publishedAt"]).encode("utf-8")
+            hasher.update(encoded_title)
+            article_id = hasher.hexdigest()
 
-        if news_collection.find_one({"article_id": article_id}) is None:
-            filtered_articles.append({key: value for key, value in article.items() if key in CHOSEN_KEYS})
-            filtered_articles[-1]["article_id"] = article_id
-        else:
-            logger.warning(f"Article {article['title']} already present in database!")
+            if news_collection.find_one({"article_id": article_id}) is None:
+                filtered_articles.append({key: value for key, value in article.items() if key in CHOSEN_KEYS})
+                filtered_articles[-1]["article_id"] = article_id
+                if article["content"]:
+                    print("=" * 80)
+                    for key, value in article.items():
+                        print(f"{key}: {value}")
 
-    # for key, value in filtered_articles[0].items():
-    #     print(f"{key}: {value}")
-    if len(filtered_articles) != 0:
-        news_collection.insert_many(filtered_articles)
+                    print("=-" * 40)
 
-else:
-    logger.error("News API doens't work!")
+                    content = get_content(article["url"])
+
+                    print("=" * 80)
+                    if content:
+                        with open(f"articles/{article_id}.txt", "w", encoding="utf-8") as f:
+                            f.write(content)
+                    else:
+                        logger.error("Content is None")
+                        continue
+            else:
+                logger.info(f"Article <{article['title']}> already present in database!")
+        
+        if len(filtered_articles) != 0:
+            news_collection.insert_many(filtered_articles)
+
+    else:
+        logger.error("News API doens't work!")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.info("Interrupted by user")
+        exit(0)
