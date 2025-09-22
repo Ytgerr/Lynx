cbea6add983b3e268ea72b1e8674c39c26502e52
Andrei <k4ty224@mail.ru>
2025-09-22 23:31:04 +0300

    add baseline model

---
+++ ml/code_app/api/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import spacy
+
+
+app = FastAPI()
+
+model = spacy.load("en_core_web_sm")
+
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/Entity-recognition/")
+async def ner(input_data: TextInput):
+    doc = model(input_data.text)
+    triplets = [{"text": ent.text, "label": ent.label_, "start": ent.start_char, "end": ent.end_char} for ent in doc.ents]
+    return {"triplets": triplets}
