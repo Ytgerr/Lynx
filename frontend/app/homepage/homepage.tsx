3067d4de2a853dbd1e56df25b1824777badbb2a9
KawaichMag <kawaichmag@gmail.com>
2025-11-25 22:00:02 +0300

    eevert "homepage decorating"
    This reverts commit 44ce16fa7268b02d8de511c04e406513db2c94ff.

--- frontend/app/homepage/homepage.tsx
+++ frontend/app/homepage/homepage.tsx
@@ -177,8 +177,9 @@ export function HomePage() {
   }
 
   useEffect(() => {
-    const fetchData = async (text_query: string) => {
-      console.log(text_query)
+    const fetchData = async () => {
+=======
+>>>>>>> 9974459 (homepage decorating)
       const res = await fetch("/ml/entity-recognition_ru", 
         {
           method: "POST",
@@ -201,6 +202,88 @@ export function HomePage() {
     fetchData("Владимир Путин немного подумав, как сообщил он, купил так давно желаемую Америку")
   }, [])
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+  const graphBaselineData: GraphData = {
+    nodes: [
+      {
+        id: 1,
+        name: "Susanna",
+        "val": 2,
+        "color": "red"
+      },
+      {
+        "id": 2,
+        "name": "Jerry",
+        "val": 7,
+        "color": "green"
+      },
+      {
+        "id": 3,
+        "name": "Shmabulok",
+        "val": 4
+      }
+    ],
+    links: [
+      {
+        "source": 1,
+        "target": 2
+      },
+      {
+        "source": 3,
+        "target": 2
+      },
+      {
+        "source": 2,
+        "target": 2
+      }
+    ]
+  }
+
+<<<<<<< HEAD
+  return (
+    <>
+      <div className="rel-cards">
+        <RelationalCard title="Lorem Ipsum" graphData={graphBaselineData}>
+          <>Voluptatem similique quibusdam dolorem! Magni debitis assumenda harum tempore, non laboriosam consequatur nihil perspiciatis sit minus ut!</>
+          <p>Nulla ut deserunt recusandae consectetur exercitationem. Tempore, id tempora doloribus nostrum odit fugiat delectus accusantium repudiandae numquam sapiente quo consectetur quasi dolores error modi reprehenderit consequatur harum iusto odio maiores!</p>
+        </RelationalCard>
+        {/* <RelationalCard title="Magni debitis" graphData={graphBaselineData}>
+          <>Consequatur nihil perspiciatis sit minus ut!</>
+          <p>Nulla ut deserunt recusandae consectetur exercitationem. Tempore, id tempora doloribus nostrum odit fugiat delectus accusantium repudiandae numquam sapiente quo consectetur quasi dolores error modi reprehenderit consequatur harum iusto odio maiores!</p>
+        </RelationalCard>
+        <RelationalCard title="Tempore" graphData={graphBaselineData}>
+          <>Sit amet consectetur adipisicing elit. Magni ratione totam aliquam saepe. Voluptatem similique quibusdam dolorem! Magni debitis assumenda harum tempore, non laboriosam consequatur nihil perspiciatis sit minus ut!</>
+          <p>Nulla ut deserunt recusandae consectetur exercitationem. Tempore, id tempora doloribus nostrum odit fugiat delectus accusantium repudiandae numquam sapiente quo consectetur quasi dolores error modi reprehenderit consequatur harum iusto odio maiores!</p>
+        </RelationalCard>
+        <RelationalCard title="Lorem Ipsum" graphData={graphBaselineData}>
+          <>Aliquam saepe. Voluptatem similique quibusdam dolorem! Magni debitis assumenda harum tempore, non laboriosam consequatur nihil perspiciatis sit minus ut!</>
+          <p>Nulla ut deserunt recusandae consectetur exercitationem. Tempore, id tempora doloribus nostrum odit fugiat delectus accusantium repudiandae numquam sapiente quo consectetur quasi dolores error modi reprehenderit consequatur harum iusto odio maiores!</p>
+        </RelationalCard>
+        <RelationalCard title="Nulla ut deserunt" graphData={graphBaselineData}>
+          <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ratione totam aliquam saepe. Voluptatem similique quibusdam dolorem! Magni debitis assumenda harum tempore, non laboriosam consequatur nihil perspiciatis sit minus ut!</>
+          <p>Nulla ut deserunt recusandae consectetur exercitationem. Tempore, id tempora doloribus nostrum odit fugiat delectus accusantium repudiandae numquam sapiente quo consectetur quasi dolores error modi reprehenderit consequatur harum iusto odio maiores!</p>
+        </RelationalCard> */}
+      </div>
+    </>
+  );
+>>>>>>> 1d43d30 (ForceGraph Integrated)
+=======
+=======
+>>>>>>> 6bfef5b (ForceGraph Integrated)
+  if (mainGraphData) {
+    return (
+      <div className="homepage">
+        <div className="rel-cards">
+          <RelationalCard title="Lorem Ipsum" graphData={mainGraphData}>
+            <p>Владимир Путин немного подумав, как сообщил он, купил так давно желаемую Америку</p>
+          </RelationalCard>
+        </div>
+      </div>
+    )
+  }
+>>>>>>> c13c213 (added integration with ml)
+=======
   return (
     <div className="homepage">
       <div className="logo-and-desc">
@@ -229,4 +312,5 @@ export function HomePage() {
       </div>
     </div>
   )
+>>>>>>> 9974459 (homepage decorating)
 }
