d05991e7270e1ef6fcf313395dc614d07d686b83
KawaichMag <kawaichmag@gmail.com>
2025-11-25 22:09:07 +0300

    Revert "added integration with ml"
    This reverts commit db4fdb5d030f161b3f1a3e9eb265dd3496748dae.

--- frontend/app/components/Graph/Graph.tsx
+++ frontend/app/components/Graph/Graph.tsx
@@ -23,9 +23,6 @@ function Graph({ graphData }: ForceGraphProps) {
                     width={500}
                     height={150}
 <<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c13c213 (added integration with ml)
                     linkThreeObjectExtend={true}
                     linkCanvasObjectMode={(() => 'after')}
                     linkCanvasObject={(link: {source: string, target: string, label: string}, ctx: any) => {
@@ -74,11 +71,8 @@ function Graph({ graphData }: ForceGraphProps) {
                         ctx.fillText(label, 0, 0);
                         ctx.restore()
                     }}
-<<<<<<< HEAD
 =======
 >>>>>>> 1d43d30 (ForceGraph Integrated)
-=======
->>>>>>> c13c213 (added integration with ml)
                 />
             </div>
         );
