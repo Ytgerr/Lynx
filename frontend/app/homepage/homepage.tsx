511a7e373cc06cd737f7b592bfc42c27a074472c
KawaichMag <kawaichmag@gmail.com>
2025-11-25 22:00:38 +0300

    Revert "added integration with ml"
    This reverts commit db4fdb5d030f161b3f1a3e9eb265dd3496748dae.

--- frontend/app/homepage/homepage.tsx
+++ frontend/app/homepage/homepage.tsx
@@ -2,34 +2,6 @@ import { RelationalCard } from "~/components/RelationalCard/RelationalCard";
 import type { GraphData } from "react-force-graph-2d";
 import "./homepage.css"
 import { useEffect, useState } from "react";
-<<<<<<< HEAD
-=======
-
-type ML_Message = {
-  results: [
-    {
-      sentence: string,
-      entities: [{
-        text: string,
-        type: string,
-        start: number,
-        stop: number
-      }],
-      relations: [
-        {
-          subject: string,
-          subject_type: string,
-          relation: string,
-          relation_type: string,
-          object: string,
-          object_type: string,
-          sentence: string
-        },
-      ]
-    }
-  ]
-}
->>>>>>> c13c213 (added integration with ml)
 
 <<<<<<< HEAD
 type ML_Message = {
@@ -144,66 +116,11 @@ export function HomePage() {
   )
 =======
 export function HomePage() {
-
-  const [mainGraphData, setMainGraphData] = useState<GraphData>();
-
-  function reformData( data: ML_Message ) {
-    let combinedDatas: GraphData = {nodes: [], links: []};
-
-    data.results.forEach(element => {
-      element.relations.forEach((relation, index) => {
-        combinedDatas.nodes.push({
-          id: relation.object + relation.relation + index,
-          name: relation.object,
-          val: 1
-        })
-
-        combinedDatas.nodes.push({
-          id: relation.subject + relation.relation + index,
-          name: relation.subject,
-          val: 1
-        })
-
-        combinedDatas.links.push({
-          source: relation.object + relation.relation + index,
-          target: relation.subject + relation.relation + index,
-          label: relation.relation
-        })
-      })
-    });
-
-    return combinedDatas
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/ml/entity-recognition_ru", 
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            "text": "Владимир Путин немного подумав, как сообщил он, купил так давно желаемую Америку"
-          })
-        }
-      )
-
-      const data = await res.json()
-      const combinedDatas = reformData(data)
-      console.log(combinedDatas)
-
-      setMainGraphData(combinedDatas)
-    }
-
-    fetchData()
-  }, [])
-
   const graphBaselineData: GraphData = {
     nodes: [
       {
-        id: 1,
-        name: "Susanna",
+        "id": 1,
+        "name": "Susanna",
         "val": 2,
         "color": "red"
       },
@@ -235,7 +152,6 @@ export function HomePage() {
     ]
   }
 
-<<<<<<< HEAD
   return (
     <>
       <div className="rel-cards">
@@ -263,24 +179,4 @@ export function HomePage() {
     </>
   );
 >>>>>>> 1d43d30 (ForceGraph Integrated)
-=======
-  if (mainGraphData) {
-    return (
-      <>
-        <div className="rel-cards">
-          <RelationalCard title="Lorem Ipsum" graphData={mainGraphData}>
-            <p>Владимир Путин немного подумав, как сообщил он, купил так давно желаемую Америку</p>
-          </RelationalCard>
-        </div>
-      </>
-    )
-    
-  } else {
-    return(
-      <>
-        Загрузка...
-      </>
-    )
-  }
->>>>>>> c13c213 (added integration with ml)
 }
